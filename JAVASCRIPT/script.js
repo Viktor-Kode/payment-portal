907bed37a34f40998bb5eaa1507da7a280e843f4
victor dosunmu <dosunmuvictor16@gmail.com>
2025-08-06 03:27:45 +0100

    initial comit

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,6 @@
+let navlinks = document.getElementById('navlinks');
+let menubar = document.getElementById('menu-icon');
+
+menubar.addEventListener('click', () =>{
+    navlinks.classList.toggle('activateNavlinks')
+})
