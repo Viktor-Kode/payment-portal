5229cc18483cc821c2bc89e13bdc5d02d25598ff
viktor <victorand804@gmail.com>
2025-08-07 01:44:56 +0100

    temp finish

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,6 +1,15 @@
 let navlinks = document.getElementById('navlinks');
 let menubar = document.getElementById('menu-icon');
 
-menubar.addEventListener('click', () =>{
-    navlinks.classList.toggle('activateNavlinks')
-})
+menubar.addEventListener('click', () => {
+    navlinks.classList.toggle('activateNavlinks');
+});
+
+// ✨ HIDE NAV MENU WHEN A LINK IS CLICKED
+let links = navlinks.querySelectorAll('a');
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        navlinks.classList.remove('activateNavlinks');
+    });
+});
